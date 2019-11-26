db3e3d0c810c7f7674d87f044341ded7f7a7e896
sot-iris <skakanos@hotmail.co.uk>
2019-11-26 21:28:25 +0000

    Update args.py

--- args.py
+++ args.py
@@ -9,7 +9,7 @@ import math
 ### Some paths
 train_file = 'Splits/train.txt'  # The path of the training txt file.
 val_file = 'Splits/val.txt'  # The path of the validation txt file.
-restore_path = './data/darknet_weights/yolov3.ckpt'  # The path of the weights to restore.
+restore_path = '/YOLOv3_TensorFlow/data/darknet_weights/yolov3.ckpt'  # The path of the weights to restore.
 save_dir = 'models/'  # The directory of the weights to save.
 log_dir = 'logs/'  # The directory to store the tensorboard log files.
 progress_log_path = 'progress.log'  # The path to record the training progress.
